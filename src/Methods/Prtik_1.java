c9fe1b7f7101a4de02ba427972296be147feb7a8
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-05-26 16:21:45 +0300

    Initial commit

---
+++ src/Methods/Prtik_1.java
@@ -0,0 +1,30 @@
+package Methods;
+
+/**
+ * ClassName: LokalDegisken
+ * Package: Methods
+ * Description:
+ *
+ * @Author Musteqqim
+ * @Create 5/26/2023 10:34 AM
+ * @Version 1.0
+ */
+public class Prtik_1 {
+    static boolean isPalindrom(int number){
+        int temp = number,reversNumber = 0,lastNumber;
+        while (temp != 0){
+            lastNumber = temp % 10;
+            reversNumber = (reversNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reversNumber)
+            return  true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(4));
+    }
+}
+
